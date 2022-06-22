d8778a1542045978c0a5dd0510c774f2b7564b11
foxxxesky <abdurrahman1467@gmail.com>
2022-06-23 00:00:05 +0800

    add jumbotron

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,25 @@
 <template>
-  <div class="container pt-5 pb-5">
-    <div class="row"></div>
+  <div class="jumbotron">
+    <div class="container pt-5 pb-5">
+      <div class="row">
+        <div class="col-7">
+          <div class="col-kiri">
+            <h4 id="text-jumbotron">
+              Shoe Cleaning Siap Membersihkan Sepatu Anda!
+            </h4>
+            <p id="desc-jumbotron">
+              Percayakan sepatu kesukaan anda kepada kami!
+            </p>
+            <nuxt-link id="button-jumbotron" class="btn btn-light" to="#"
+              >Bersihkan Sekarang!</nuxt-link
+            >
+          </div>
+        </div>
+        <div class="col-5">
+          <img id="img-jumbotron" src="images/char.png" alt="" />
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -13,3 +32,36 @@ export default {
   },
 };
 </script>
+
+<style>
+.jumbotron {
+  background-color: var(--primary);
+}
+
+.col-kiri {
+  padding-top: 80px;
+}
+
+#text-jumbotron {
+  font-size: 38px;
+  font-weight: var(--semi-bold);
+  color: var(--white);
+}
+
+#desc-jumbotron {
+  font-size: 14px;
+  font-weight: var(--medium);
+  color: var(--white);
+}
+
+#img-jumbotron {
+  position: absolute;
+  height: 400px;
+}
+
+#button-jumbotron {
+  font-size: 20px;
+  font-weight: var(--semi-bold);
+  color: var(--primary);
+}
+</style>
