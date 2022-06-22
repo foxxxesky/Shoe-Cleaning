3691217a5517242f43dbaef2055763c4d7c5ecf4
foxxxesky <abdurrahman1467@gmail.com>
2022-06-23 00:10:09 +0800

    fix jumbotron

--- pages/index.vue
+++ pages/index.vue
@@ -1,25 +1,35 @@
 <template>
-  <div class="jumbotron">
-    <div class="container pt-5 pb-5">
-      <div class="row">
-        <div class="col-7">
-          <div class="col-kiri">
-            <h4 id="text-jumbotron">
-              Shoe Cleaning Siap Membersihkan Sepatu Anda!
-            </h4>
-            <p id="desc-jumbotron">
-              Percayakan sepatu kesukaan anda kepada kami!
-            </p>
-            <nuxt-link id="button-jumbotron" class="btn btn-light" to="#"
-              >Bersihkan Sekarang!</nuxt-link
-            >
+  <div class="content">
+    <!-- Jumbotron -->
+    <div class="jumbotron">
+      <div class="container pt-5 pb-5">
+        <div class="row">
+          <div class="col-7">
+            <div class="col-kiri pb-2">
+              <h4 id="text-jumbotron">
+                Shoe Cleaning Siap Membersihkan Sepatu Anda!
+              </h4>
+              <p id="desc-jumbotron">
+                Percayakan sepatu kesukaan anda kepada kami!
+              </p>
+              <nuxt-link id="button-jumbotron" class="btn btn-light" to="#"
+                >Bersihkan Sekarang!</nuxt-link
+              >
+            </div>
           </div>
-        </div>
-        <div class="col-5">
-          <img id="img-jumbotron" src="images/char.png" alt="" />
+          <div class="col-5">
+            <img id="img-jumbotron" src="images/char.png" alt="" />
+          </div>
         </div>
       </div>
     </div>
+    <!-- Jumbotron -->
+
+    <!-- Layanan -->
+    <div class="container">
+      <h4 class="text-center pt-5" id="text-layanankami">Layanan Kami</h4>
+    </div>
+    <!-- Layanan -->
   </div>
 </template>
 
